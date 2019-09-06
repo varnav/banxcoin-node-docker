d3243fe58055f7670b65380622621126ce4b886a
Evgeny Varnavskiy <evarnavsky@bytewerk.com>
2019-09-06 10:11:21 -0400

    Modifications to config file

--- bin/setup.sh
+++ bin/setup.sh
@@ -5,10 +5,10 @@ if [ ! -f "$HOME/.banxcoin" ]; then
 echo'
 rpcuser=rpc_banxcoin
 rpcpassword=69c863e3356d3daee95df454a1
-rpcallowip=127.0.0.1
+rpcallowip=0.0.0.0
 listen=1
 server=1
 txindex=1
-daemon=1
+daemon=0
 ' > $HOME/.banxcoin
 fi
